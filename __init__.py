2df9acad4f66e1dcb9b969d301b6ad56148ef3df
Manish <140468228+Kyaamatq@users.noreply.github.com>
2023-12-25 12:58:01 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,28 +1,9 @@
-from DAXXMUSIC.core.bot import DAXX
-from DAXXMUSIC.core.dir import dirr
-from DAXXMUSIC.core.git import git
-from DAXXMUSIC.core.userbot import Userbot
-from DAXXMUSIC.misc import dbb, heroku
-
-from SafoneAPI import SafoneAPI
-from .logging import LOGGER
-
-dirr()
-git()
-dbb()
-heroku()
-
-app = DAXX()
-api = SafoneAPI()
-userbot = Userbot()
-
-
-from .platforms import *
-
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
-YouTube = YouTubeAPI()
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .extraction import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
+from .imposterdb import *
